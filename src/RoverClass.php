5b9affee49fd5a75619f262483b60612d9699147
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2019-06-12 15:52:51 +0200

    Blue phase of initialization method creation

--- src/RoverClass.php
+++ src/RoverClass.php
@@ -11,12 +11,33 @@ namespace App;
  */
 class RoverClass
 {
+    /**
+     * X coordinate of rover.
+     *
+     * @var int $xCoordinate
+     */
     private $xCoordinate;
+
+    /**
+     * Y coordinate of rover.
+     *
+     * @var int $yCoordinate
+     */
     private $yCoordinate;
+
+    /**
+     * Direction of rover.
+     *
+     * @var string $direction
+     */
     private $direction;
 
     /**
      * RoverClass constructor.
+     *
+     * @param int    $xCoordinate X coordinate of Rover
+     * @param int    $yCoordinate Y coordinate of Rover
+     * @param string $direction   Initial direction of Rover (N, S, W, E)
      */
     public function __construct(int $xCoordinate, int $yCoordinate, string $direction)
     {
@@ -25,11 +46,21 @@ class RoverClass
         $this->direction = $direction;
     }
 
+    /**
+     * Get coordinates of rover.
+     *
+     * @return array Coordinates of rover
+     */
     public function getCoordinates()
     {
         return ['x' => $this->xCoordinate, 'y' => $this->yCoordinate];
     }
 
+    /**
+     * Get direction of rover.
+     *
+     * @return string Direction of rover
+     */
     public function getDirection()
     {
         return $this->direction;
