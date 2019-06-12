0ba21df36bb3766a3799744668ef9cf752a132df
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2019-06-12 15:28:56 +0200

    Green phase of initialization method creation

--- src/RoverClass.php
+++ src/RoverClass.php
@@ -11,10 +11,27 @@ namespace App;
  */
 class RoverClass
 {
+    private $xCoordinate;
+    private $yCoordinate;
+    private $direction;
+
     /**
      * RoverClass constructor.
      */
-    public function __construct()
+    public function __construct(int $xCoordinate, int $yCoordinate, string $direction)
     {
+        $this->xCoordinate = $xCoordinate;
+        $this->yCoordinate = $yCoordinate;
+        $this->direction = $direction;
+    }
+
+    public function getCoordinates()
+    {
+        return ['x' => $this->xCoordinate, 'y' => $this->yCoordinate];
+    }
+
+    public function getDirection()
+    {
+        return $this->direction;
     }
 }
