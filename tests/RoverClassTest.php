dfb817a9c2b3e1e15d034932fba670c28c32c263
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2019-06-27 15:34:46 +0200

    Red phase of learning rover go backward

--- tests/RoverClassTest.php
+++ tests/RoverClassTest.php
@@ -46,4 +46,20 @@ class RoverClassTest extends TestCase
         $this->assertSame($roverClassObject->getDirection(), 'N');
     }
 
+    /**
+     * Test whether rover can react on commands sent by array of characters.
+     */
+    public function testRoverCanGoBackward()
+    {
+        //given
+        $roverClassObject = new RoverClass(0, 0, "N");
+
+        //when
+        $roverClassObject->executeCommands(['b']);
+
+        //then
+        $this->assertSame($roverClassObject->getX(), 0);
+        $this->assertSame($roverClassObject->getY(), -1);
+        $this->assertSame($roverClassObject->getDirection(), 'N');
+    }
 }
