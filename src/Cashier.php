10d4a78eec745ecbfa849a2eea1330cf2f156231
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2019-10-18 12:01:05 +0200

    Create cashier kata

--- src/Cashier.php
+++ src/Cashier.php
@@ -7,6 +7,32 @@ class Cashier
 {
     function getOrder($input)
     {
-        return "";
+        $result = '';
+
+        $matchedMenuItems = [];
+        preg_match_all('/burger|fries|chicken|pizza|sandwich|onionrings|milkshake|coke/', $input, $matchedMenuItems);
+
+        $currentItems = [
+            'burger' => 0,
+            'fries' => 0,
+            'chicken' => 0,
+            'pizza' => 0,
+            'sandwich' => 0,
+            'onionrings' => 0,
+            'milkshake' => 0,
+            'coke' => 0,
+        ];
+
+        foreach ($matchedMenuItems[0] as $item) {
+            $currentItems[$item] += 1;
+        }
+        $results = [];
+        foreach ($currentItems as $itemName => $numberOfOccurrences) {
+            for ($i = 0; $i < $numberOfOccurrences; $i++) {
+                $results[] = ucfirst($itemName);
+            }
+        }
+
+        return implode(" ", $results);
     }
 }
--- tests/CashierTest.php
+++ tests/CashierTest.php
@@ -2,15 +2,110 @@
 
 namespace Tests;
 
+use App\Cashier;
 use PHPUnit\Framework\TestCase;
 
 class CashierTest extends TestCase
 {
     /**
-     *
+     * @dataProvider  dataProviderForTestOneStringCapitalisation
      */
-    public function test()
+    public function testOneStringCapitalisation($input, $output)
     {
-        $this->assertTrue(true);
+        $cashier = new Cashier();
+
+        $order = $cashier->getOrder($input);
+
+        $this->assertEquals($output, $order);
     }
+
+    public function dataProviderForTestOneStringCapitalisation()
+    {
+        yield [
+            'burger',
+            'Burger',
+        ];
+        yield [
+            'fries',
+            'Fries',
+        ];
+        yield [
+            'chicken',
+            'Chicken',
+        ];
+        yield [
+            'pizza',
+            'Pizza',
+        ];
+        yield [
+            'sandwich',
+            'Sandwich',
+        ];
+        yield [
+            'onionrings',
+            'Onionrings',
+        ];
+        yield [
+            'milkshake',
+            'Milkshake',
+        ];
+        yield [
+            'coke',
+            'Coke',
+        ];
+    }
+
+    /**
+     * @dataProvider  dataProviderForTestReturnSeparateWordsWithoutOrdering
+     */
+    public function testReturnSeparateWordsWithoutOrdering($input, $output)
+    {
+        $cashier = new Cashier();
+
+        $order = $cashier->getOrder($input);
+
+        $this->assertEquals($output, $order);
+    }
+
+    public function dataProviderForTestReturnSeparateWordsWithoutOrdering()
+    {
+        yield [
+            'burgerburger',
+            'Burger Burger',
+        ];
+        yield [
+            'burgerburgerburger',
+            'Burger Burger Burger',
+        ];
+        yield [
+            'burgerfries',
+            'Burger Fries',
+        ];
+        yield [
+            'burgerfrieschickenpizzasandwichonionringsmilkshakecoke',
+            'Burger Fries Chicken Pizza Sandwich Onionrings Milkshake Coke',
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForTestReturnSortedMenuItems
+     */
+    public function testReturnSortedMenuItems($input, $output)
+    {
+        $cashier = new Cashier();
+
+        $order = $cashier->getOrder($input);
+
+        $this->assertEquals($output, $order);
+    }
+
+    public function dataProviderForTestReturnSortedMenuItems()
+    {
+        yield [
+            'friesburger',
+            'Burger Fries',
+        ];
+    }
+
+
 }
