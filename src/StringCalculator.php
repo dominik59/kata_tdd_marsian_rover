c9fd5e04d70a4a4953e8b349a44e1efd8ec056ba
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2020-02-10 13:17:51 +0100

    First stage of resolving

---
+++ src/StringCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App;
+
+
+class StringCalculator
+{
+    public function add(string $string): int
+    {
+
+    }
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use App\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+test
+}
