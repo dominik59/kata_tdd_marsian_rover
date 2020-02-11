aee0de2ae80a00e46279463d8c1219e5473d15c7
Dominik Pawlik <dominik.pawlik@softwaremind.com>
2020-02-11 10:35:59 +0100

    Make a refactored solution

--- src/SupplierGrouper.php
+++ src/SupplierGrouper.php
@@ -6,8 +6,112 @@ namespace App;
 
 class SupplierGrouper
 {
-    public function group(array $rawCollection)
+    public function group(array $rawCollection): array
     {
+        $closestSuppliersWithoutGroup = $this->getClosestSuppliersWithoutGroup($rawCollection);
+        $closestSuppliersFromGroup = $this->getClosestGroupedSupplier($rawCollection);
 
+        return array_merge($closestSuppliersFromGroup, array_values($closestSuppliersWithoutGroup));
+    }
+
+    private function getClosestGroupedSupplier(array $rawCollection): array
+    {
+        $groupedSuppliers = $this->getGroupedSupplierWithClosestBranch($rawCollection);
+
+        return $this->getClosestSupplierFromGroup($groupedSuppliers);
+    }
+
+    private function getClosestSuppliersWithoutGroup(array $rawCollection): array
+    {
+        $closestSuppliersWithoutGroup = [];
+
+        foreach ($rawCollection as $supplier) {
+            $groupId = $supplier['group'];
+            $supplierId = $supplier['id'];
+
+            if ($groupId === null) {
+                if (isset($closestSuppliersWithoutGroup[$supplierId])) {
+                    $closestSuppliersWithoutGroup[$supplierId] = $this->getCloserSupplier(
+                        $supplier,
+                        $closestSuppliersWithoutGroup[$supplierId]
+                    );
+                } else {
+                    $closestSuppliersWithoutGroup[$supplierId] = $supplier;
+                }
+            }
+        }
+
+        return $closestSuppliersWithoutGroup;
+    }
+
+    private function getGroupedSupplierWithClosestBranch(array $rawCollection): array
+    {
+        $groupedSuppliers = [];
+
+        foreach ($rawCollection as $supplier) {
+            $groupId = $supplier['group'];
+            $supplierId = $supplier['id'];
+
+            if ($groupId !== null) {
+                if (isset($groupedSuppliers[$groupId][$supplierId])) {
+                    $groupedSuppliers[$groupId][$supplierId] = $this->getCloserSupplier(
+                        $supplier,
+                        $groupedSuppliers[$groupId][$supplierId]
+                    );
+                } else {
+                    $groupedSuppliers[$groupId][$supplierId] = $supplier;
+                }
+            }
+        }
+
+        return $groupedSuppliers;
+    }
+
+    private function getCloserSupplier(array $firstSupplier, array $secondSupplier): array
+    {
+        if ($this->extractDistanceFromSupplier($firstSupplier) < $this->extractDistanceFromSupplier($secondSupplier)) {
+            $closerSupplier = $firstSupplier;
+        } else {
+            $closerSupplier = $secondSupplier;
+        }
+
+        return $closerSupplier;
+    }
+
+    private function extractDistanceFromSupplier(array $supplier)
+    {
+        return $supplier['distance'];
+    }
+
+    /**
+     * @param array $groupedSuppliers
+     *
+     * @return array
+     */
+    private function getClosestSupplierFromGroup(array $groupedSuppliers): array
+    {
+        $closestSuppliersFromGroup = [];
+        foreach ($groupedSuppliers as $suppliersInsideGroup) {
+            $closestSupplier = null;
+
+            if (count($suppliersInsideGroup) === 1) {
+                $closestSupplier = $suppliersInsideGroup[array_key_first($suppliersInsideGroup)];
+            } else {
+                foreach ($suppliersInsideGroup as $supplier) {
+                    if ($closestSupplier === null) {
+                        $closestSupplier = $supplier;
+                    } else {
+                        $closestSupplier = $this->getCloserSupplier(
+                            $supplier,
+                            $closestSupplier
+                        );
+                    }
+                }
+            }
+
+            $closestSuppliersFromGroup[] = $closestSupplier;
+        }
+
+        return $closestSuppliersFromGroup;
     }
 }
--- tests/SupplierGrouperTest.php
+++ tests/SupplierGrouperTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 
 class SupplierGrouperTest extends TestCase
 {
-    public function testReturnJustOneSupplierWhenOneProvided()
+    public function testReturnJustOneSupplierWhenOneProvided(): void
     {
         //given
         $rawCollection = [
@@ -27,9 +27,125 @@ class SupplierGrouperTest extends TestCase
         //then
         $this->assertSame(
             [
+                [
+                    'id' => 1000,
+                    'group' => null,
+                    'distance' => 1,
+                ],
+            ],
+            $groupedSuppliers
+        );
+    }
+
+    public function testSupplierWithBranchShouldBecomeJustClosestOne(): void
+    {
+        //given
+        $rawCollection = [
+            [
                 'id' => 1000,
                 'group' => null,
                 'distance' => 1,
+            ],
+            [
+                'id' => 1000,
+                'group' => null,
+                'distance' => 5,
+            ],
+        ];
+        $supplierGrouper = new SupplierGrouper();
+
+        //when
+        $groupedSuppliers = $supplierGrouper->group($rawCollection);
+
+        //then
+        $this->assertSame(
+            [
+                [
+                    'id' => 1000,
+                    'group' => null,
+                    'distance' => 1,
+                ],
+            ],
+            $groupedSuppliers
+        );
+    }
+
+    public function testGroupedSuppliersWithoutBranchesShouldReturnOneClosestInGroup(): void
+    {
+        //given
+        $rawCollection = [
+            [
+                'id' => 1000,
+                'group' => 10,
+                'distance' => 1,
+            ],
+            [
+                'id' => 1001,
+                'group' => 10,
+                'distance' => 5,
+            ],
+        ];
+        $supplierGrouper = new SupplierGrouper();
+
+        //when
+        $groupedSuppliers = $supplierGrouper->group($rawCollection);
+
+        //then
+        $this->assertSame(
+            [
+                [
+                    'id' => 1000,
+                    'group' => 10,
+                    'distance' => 1,
+                ],
+            ],
+            $groupedSuppliers
+        );
+    }
+
+    public function testGroupedSuppliersWithBranchesShouldReturnOneClosestInGroupAndInBranch(): void
+    {
+        //given
+        $rawCollection = [
+            [
+                'id' => 1000,
+                'group' => 10,
+                'distance' => 1,
+            ],
+            [
+                'id' => 1000,
+                'group' => 10,
+                'distance' => 7,
+            ],
+            [
+                'id' => 1001,
+                'group' => 11,
+                'distance' => 5,
+            ],
+            [
+                'id' => 1001,
+                'group' => 11,
+                'distance' => 8,
+            ],
+        ];
+        $supplierGrouper = new SupplierGrouper();
+
+        //when
+        $groupedSuppliers = $supplierGrouper->group($rawCollection);
+
+        //then
+        $this->assertSame(
+            [
+                [
+                    'id' => 1000,
+                    'group' => 10,
+                    'distance' => 1,
+                ],
+                [
+                    'id' => 1001,
+                    'group' => 11,
+                    'distance' => 5,
+                ],
             ],
             $groupedSuppliers
         );
